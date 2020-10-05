644bda204fef12c16c0dc7bff206e7f38d96f946
山崎孝洋 <yamazakitakahiro@yamazakitakahironoMacBook-Pro.local>
2020-10-05 17:04:57 +0900

    to Gin

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// buildフォルダを公開
-	http.Handle("/", http.FileServer(http.Dir("./radiohub/build")))
 
-	// 前述のhomepageを記述してないと下の記述でしか動かない。他のパスだとバグる
-	// http.Handle("/", http.FileServer(http.Dir("./build")))
+	r := gin.Default()
+	r.Static("/", "./radiohub/build")
+	r.Run()
 
-	fmt.Println("Server Started Port 8080")
-	http.ListenAndServe(":8080", nil)
 }
