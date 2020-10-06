840518d4653b839bb627f1acc878faf25d7ef513
山崎孝洋 <yamazakitakahiro@yamazakitakahironoMacBook-Pro.local>
2020-10-06 23:26:07 +0900

    add area api

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	r := gin.Default()
-	r.Static("/", "./radiohub/build")
+	r.Use(static.Serve("/", static.LocalFile("radiohub/build", false)))
+	r.NoRoute(func(c *gin.Context) { c.File("radiohub/build/index.html") })
+	r.GET("/area", getArea)
 	r.Run()
 
+}
+
+func getArea(c *gin.Context) {
+	url := "http://radiko.jp/v3/station/list/JP13.xml"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	c.String(200, string(data))
 }
