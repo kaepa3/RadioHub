54b657cc6f4cd7dd8e83c4662ce1b570297ddd5f
山崎孝洋 <yamazakitakahiro@yamazakitakahironoMacBook-Pro.local>
2020-10-07 23:53:22 +0900

    create list

--- radiohub/src/App.tsx
+++ radiohub/src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var parser = require('xml-js');
+
+class App extends React.Component {
+  componentDidMount() {
+    fetch("/area")
+      .then(function(response) {
+        console.log('1nd')
+        return response.body?.getReader().read()
+      })
+      .then(function(response) {
+        console.log('2nd')
+        var xml = (new TextDecoder()).decode(response?.value)
+        var options = { ignoreComment: true, alwaysChildren: true };
+        var data = parser.xml2js(xml, options)['elements'][0]['elements']
+
+        console.log(data[0])
+        var list = document.getElementById('channel');
+        data.forEach((v: any) => {
+          var name = v.elements[0].elements[0]['text']
+          let option = document.createElement('option');
+          option.innerHTML =   name;
+          list?.appendChild(option);
+        })
+
+      })
+      .catch((err) => {
+        console.log('err' + err)
+      })
+  }
+
+  handleClick() {
+    console.log('The link was clicked.');
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <select id="channel"></select>
+            <datalist>
+            </datalist>
+            <button onClick={this.handleClick}>rec</button>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
